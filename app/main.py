dfe95ac66ec1a0cb46a5a34a4398c66bff91de9e
Luna <ton-email@example.com>
2026-01-10 17:34:43 +0400

    added SQL trucs

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from typing import Optional
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import time
 
 # func for retrieve post by id:
 def find_post_by_id(id:int):
@@ -36,18 +37,20 @@ my_posts = [
 
 
 # SQL
-try:
-    conn = psycopg2.connect(host= "localhost",
-                    database= "course_fastapi",
-                    user= "postgres",
-                    password= "luna",
-                    cursor_factory= RealDictCursor,
-                    )
-    cursor = conn.cursor()
-    print("database connected!")
-
-except Exception as e:
-    print(f"ERROR db connect: {e}")
+while True:
+    try:
+        conn = psycopg2.connect(host= "localhost",
+                        database= "course_fastapi",
+                        user= "postgres",
+                        password= "luna",
+                        cursor_factory= RealDictCursor,
+                        )
+        cursor = conn.cursor()
+        print("database connected!")
+        break
+    except Exception as e:
+        print(f"ERROR db connect: {e}")
+        time.sleep(2)
 
 
 
@@ -68,14 +71,17 @@ async def root():
 # GET all
 @app.get("/posts")
 async def get_all_posts():
-    return {"data":my_posts}
+    cursor.execute("SELECT * FROM posts;")
+    posts = cursor.fetchall()
+    print(posts)
+    return {"data":posts}
 
 # CREATE post
 @app.post("/posts", status_code= status.HTTP_201_CREATED)
 async def create_post(new_post: Post ):
-    post_dict = new_post.model_dump()
-    post_dict["id"] = randrange(0,10000000)
-    my_posts.append(post_dict) 
+    cursor.execute("""INSERT INTO posts (title, content) VALUES (%s, %s)""",
+                   (new_post.title, new_post.content))
+    conn.commit()
     return {"new_post": new_post}
 
 # GET latest post
@@ -88,26 +94,27 @@ async def get_latest_post():
 # GET post by ID
 @app.get("/posts/{id}")
 def get_post_detail(id:int):
-    post = find_post_by_id(id)
-    if not post:
+    cursor.execute("SELECT * FROM posts WHERE id=%s", (id,))
+    post = cursor.fetchone()
+    if post is None:
         raise HTTPException(
-                status_code = status.HTTP_404_NOT_FOUND,
-                detail = f"the post with ID:{id} NOT FOUND!" 
-                )
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id ID={id} NOT FOUND!"
+        )
     return {"post_detail": post } 
 
 
 # DELETE a post by ID:
 @app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
 async def delete_post_by_id(id:int):
-    post_to_delete = find_post_by_id(id)
+    cursor.execute("DELETE FROM posts WHERE id=%s RETURNING *", (id,))
+    post_to_delete = cursor.fetchone()
     if not post_to_delete:
         raise HTTPException(
                 status_code = status.HTTP_404_NOT_FOUND,
                 detail = f"the post with ID:{id} NOT FOUND!" 
                 )
-    # faire le delete propre:
-    my_posts.remove(post_to_delete)
+    conn.commit()
     return
  
 # UPDATE a post by id:
